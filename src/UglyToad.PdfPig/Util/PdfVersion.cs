1ee0650fd3f705f6c60942770df8f7d87f107c9b
Jörg Nowak <kontakt@nowak-webdesign.de>
2023-04-03 07:22:55 +0200

    Add PdfVersion
    Exposes PdfPig version.

---
+++ src/UglyToad.PdfPig/Util/PdfVersion.cs
@@ -0,0 +1,18 @@
+﻿namespace UglyToad.PdfPig.Util
+{
+    using System.Reflection;
+
+    /// <summary>
+    /// Exposes PdfPig version.
+    /// </summary>
+    public class PdfVersion
+    {
+        /// <summary>
+        /// Returns the version of PdfPig.
+        /// </summary>
+        public static string GetVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString(3);
+        }
+    }
+}
