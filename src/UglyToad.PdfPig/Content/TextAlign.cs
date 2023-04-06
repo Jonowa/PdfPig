e4bdf97f68569532cbf86849afa5177ccf15cb08
Jörg Nowak <kontakt@nowak-webdesign.de>
2023-04-06 22:59:04 +0200

    Add horizontal text alignment

---
+++ src/UglyToad.PdfPig/Content/TextAlign.cs
@@ -0,0 +1,33 @@
+﻿namespace UglyToad.PdfPig.Content
+{
+    /// <summary>
+    /// Alignment of the text.
+    /// </summary>
+    public enum TextAlign
+    {
+        /// <summary>
+        /// Text aligned to the beginning of the line.
+        /// </summary>
+        Left,
+
+        /// <summary>
+        /// Text aligned to the center of the line.
+        /// </summary>
+        Center,
+
+        /// <summary>
+        /// Text aligned to the end of the line.
+        /// </summary>
+        Right,
+
+        /// <summary>
+        /// Text aligned to the beginning of the line.
+        /// </summary>
+        Start,
+
+        /// <summary>
+        /// Text aligned to the end of the line.
+        /// </summary>
+        End
+    }
+}
